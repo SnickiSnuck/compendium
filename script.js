195487c588880feed873c65d58aa071a5a5beed1
Leo <161974844+SnickiSnuck@users.noreply.github.com>
2026-03-04 06:51:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const scrollButton = document.querySelector('.scroll-button');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) { // Erscheint nach 200px Scrollweg
+    scrollButton.classList.add('visible');
+  } else {
+    scrollButton.classList.remove('visible');
+  }
+});
+
+scrollButton.onclick = function() {
+  window.scrollTo({
+    top: 0,
+  });
+};
+
 function toggleCover(colIndex) {
     // Wir suchen alle td-Elemente, die an der Position colIndex in ihrer Zeile stehen
     const cells = document.querySelectorAll(`tbody td:nth-child(${colIndex})`);
